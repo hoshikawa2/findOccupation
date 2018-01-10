a98e7115d4bfdabc072b0721925e2cf26219bbb2
Cristiano Hoshikawa <hoshikawa@uol.com.br>
2018-01-10 09:24:49 -0200

    teste

--- src/main/java/findOccupation/Greeting.java
+++ src/main/java/findOccupation/Greeting.java
@@ -1,20 +1,30 @@
 package findOccupation;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Greeting {
 
-    private final long id;
-    private final String content;
+    private String id;
+    private String content;
 
-    public Greeting(long id, String content) {
-        this.id = id;
-        this.content = content;
+    public Greeting() {
     }
 
-    public long getId() {
+    public String getId() {
         return id;
     }
 
     public String getContent() {
         return content;
     }
+    
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id='" + id + '\'' +
+                ", content='" + content + '\'' + 
+                '}';
+    }    
 }
